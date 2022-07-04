b6010e721b18968d6cea28f8d8a6518a5315d422
Nocthonax Arts <nocthonaxarts@gmail.com>
2022-07-04 10:22:07 +0200

    Delete SRC/Underwater_Image_Color_Restoration directory

--- SRC/Underwater_Image_Color_Restoration/GBdehazingRCorrection/getAdaptiveSceneRadiance.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def AdaptiveSceneRadiance(sceneRadiance,S_x):
-    sceneRadiance = np.float64(sceneRadiance)
-    sceneRadiance  = sceneRadiance * S_x
-
-    sceneRadiance = np.clip(sceneRadiance, 0, 255)
-    sceneRadiance = np.uint8(sceneRadiance)
-    return sceneRadiance
--- SRC/Underwater_Image_Color_Restoration/LowComplexityDCP/TransmissionMap.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import cv2
-def TransmissionComposition(folder, transmission, number, param):
-    transmission = (transmission * 255).astype(np.uint8)
-    cv2.imwrite(folder+ 'TansmissionMap/'+ number + param + '_LowComplexityDCP.jpg', transmission)
